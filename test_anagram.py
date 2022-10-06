5cbc9298a1e806917dcac13b4f818926c818d59a
Antonia <56763757+antoniapriesnitz@users.noreply.github.com>
2022-10-06 13:35:16 +0200

    add tests for removal of non-alphabetic characters and conversion to lower case.

--- test_anagram.py
+++ test_anagram.py
@@ -4,9 +4,17 @@ from anagram import Anagram
 class TestAnagram(unittest.TestCase):
 
     def test_word(self):
-        anagram = Anagram('remove w h i t e s p a c e', 'german.txt')
+        anagram = Anagram('remove \t\n\rw h i t e s p a c e', 'german.txt')
         word = anagram.word
         self.assertEqual(word, 'removewhitespace')
+
+        anagram = Anagram('only,.%@-_?!´#+~*1^<>|letters', 'german.txt')
+        word = anagram.word
+        self.assertEqual(word, 'onlyletters')
+
+        anagram = Anagram('LoWeRcAsE', 'german.txt')
+        word = anagram.word
+        self.assertEqual(word, 'lowercase')
 
 
 
