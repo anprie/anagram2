a87895ac8cf5efa563ba384a8e70aeaf20f1b7ea
Antonia <56763757+antoniapriesnitz@users.noreply.github.com>
2022-10-06 18:15:20 +0200

    pass argument word to letters and count functions so that we can use them for syllables and clusters, too.

--- anagram.py
+++ anagram.py
@@ -12,9 +12,9 @@ class Anagram:
     def __str__(self):
         return f"word: {self.word}\ninventory: {self.inventory}\nlanguage: {self.language}"
 
-    def letters(self):
+    def letters(self, word):
         letters = {}
-        L = list(self.word)
+        L = list(word)
         for l in L:
             if l in letters:
                 letters[l] +=1
@@ -22,17 +22,10 @@ class Anagram:
                 letters[l] = 1
         return letters
 
-#    def inventory(self):
-#        letter_set = set(list(self.word))
-#        print("letter set: ",letter_set)
-#        self.inventory = sorted(list(letter_set))
-#        print("inventory: ", self.inventory)
-#        return inventory
-
-    def count(self):
+    def count(self, word):
         inventory = self.inventory
         count = [0 for l in inventory]
-        for l in self.word:
+        for l in word:
             for i in range(len(self.inventory)):
                 if l == self.inventory[i]:
                     count[i]+=1
