c9cff2312693f9e0ccf1769e5ee2632087623158
Antonia <56763757+antoniapriesnitz@users.noreply.github.com>
2022-10-08 19:48:50 +0200

    test for read method.

--- test_language.py
+++ test_language.py
@@ -9,6 +9,14 @@ class TestLanguage(unittest.TestCase):
         language = Language(lang_file)
         self.assertEqual(language.name, "german")
 
+    def test_read(self):
+        language = Language("smurf.txt")
+        language.read()
+        self.assertEqual(language.onset, {'s','m','r','f','fr','sm'})
+        self.assertEqual(language.nucleus, {'u'})
+        self.assertEqual(language.coda, {'s','m','r','f','rm','rf','rs','sm','mf'})
+
+
 if __name__ == '__main__':
     unittest.main()
 
