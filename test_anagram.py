e8daeda2451ffdea0b5eb2e48074cf75e0d22970
Antonia <56763757+antoniapriesnitz@users.noreply.github.com>
2022-10-08 14:31:35 +0200

    better words for testing.

--- test_anagram.py
+++ test_anagram.py
@@ -1,55 +1,27 @@
 import unittest
 from anagram import Anagram
-
+from word import Word
 class TestAnagram(unittest.TestCase):
 
-    def test_word(self):
-        anagram = Anagram('remove \t\n\rw h i t e s p a c e', 'german.txt')
-        print(anagram)
-        word = anagram.word
-        self.assertEqual(word, 'removewhitespace')
-
-        anagram = Anagram('only,.%@-_?!´#+~*1^<>|letters', 'german.txt')
-        print(anagram)
-        word = anagram.word
-        self.assertEqual(word, 'onlyletters')
-
-        anagram = Anagram('LoWeRcAsE', 'german.txt')
-        print(anagram)
-        word = anagram.word
-        self.assertEqual(word, 'lowercase')
-
-        anagram = Anagram('Ümläuteß', 'german.txt')
-        print(anagram)
-        word = anagram.word
-        self.assertEqual(word, 'ümläuteß')
-
     def test_inventory(self):
-        anagram = Anagram('dcbaABCD', 'german.txt')
+        word = Word('dcbaABCD')
+        anagram = Anagram(word, 'german.txt')
         print(anagram)
-        inventory = anagram.inventory
-        self.assertEqual(inventory,['a','b','c','d'])
+        self.assertEqual(anagram.inventory,['a','b','c','d'])
 
     def test_count(self):
-        word = 'countletters'
+        word = Word('bcbcba')
         anagram = Anagram(word, 'german.txt')
         print(anagram)
-        count = anagram.count(word)
-        self.assertEqual(count, [1,2,1,1,1,1,1,3,1])
+        self.assertEqual(anagram.count, [1,3,2])
 
-    def test_letters(self):
-        word = 'countletters'
+    def test_subtract(self):
+        word = Word('bcbcba')
         anagram = Anagram(word, 'german.txt')
-        print(anagram)
-        letters = anagram.letters(word)
-        self.assertEqual(letters, {'c':1,'e':2,'l':1,'n':1,'o':1,'r':1,'s':1,'t':3,'u':1})
-
-    def test_difference(self):
-        anagram = Anagram('dcbaABCD', 'german.txt')
-        print(anagram)
-        remaining = anagram.difference('cbDa')
-        self.assertEqual(remaining,[1,1,1,1])
-        
+        word2 = Word('bcbxy')
+        (difference, spare) = anagram.subtract(word2)
+        self.assertEqual(difference, [1,1,1])
+        self.assertEqual(spare, ['x','y'])
 
 
 if __name__ == '__main__':
