cda84907b771ebe14e6385554b52cfdfba3e4415
Antonia <56763757+antoniapriesnitz@users.noreply.github.com>
2022-10-08 14:35:32 +0200

    new method subtract which subtracts the letters occurring in the input word from the count array.

--- anagram.py
+++ anagram.py
@@ -1,14 +1,31 @@
 import re
+import copy
 
 class Anagram:
 
     def __init__(self, word, language_file):
         self.word = word
         self.file = language_file
-        self.language = language.split(sep='.', maxsplit=1)[0]
+        self.language = language_file.split(sep='.', maxsplit=1)[0]
+        self.inventory = sorted(word.letters.keys())
+        self.count = [word.letters[key] for key in sorted(word.letters.keys())]
 
     def __str__(self):
-        return f"word: {self.word}\nfile: {self.file}\nlanguage: {self.language}"
+        return f"word: {self.word.word}\nfile: {self.file}\nlanguage: {self.language}"
+
+    def subtract(self,word):
+        result = copy.deepcopy(self.count)
+        not_in_inventory = []
+        for l in word.word:
+            contains_letter = False
+            for i in range(len(self.inventory)):
+                if l == self.inventory[i]:
+                    result[i]-=1
+                    contains_letter = True
+                    break
+            if not contains_letter:
+                not_in_inventory.append(l)
+        return (result, not_in_inventory)
 
     def read(self):
         # declare three empty sets (onset, nucleus, coda)
