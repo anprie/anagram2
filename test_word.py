935c0a755e092f7534daa8650d5fd479975202c3
Antonia <56763757+antoniapriesnitz@users.noreply.github.com>
2022-10-08 13:48:51 +0200

    remove obsolete tests.

--- test_word.py
+++ test_word.py
@@ -16,22 +16,6 @@ class TestWord(unittest.TestCase):
         word = Word('Ümläuteß')
         self.assertEqual(word.word, 'ümläuteß')
 
-    def test_inventory(self):
-        word = Word('dcbaABCD')
-        inventory = word.inventory
-        self.assertEqual(word.inventory,['a','b','c','d'])
-
-    def test_count(self):
-        word = Word('countletters')
-        (count, spare) = word.count(word.word)
-        self.assertEqual(count, [1,2,1,1,1,1,1,3,1])
-        self.assertEqual(spare, [])
-
-        word2 = Word('countxy')
-        (count, spare) = word.count(word2.word)
-        self.assertEqual(count, [1,0,0,1,1,0,0,1,1])
-        self.assertEqual(spare, ['x','y'])
-
     def test_letters(self):
         word = Word('countletters')
         self.assertEqual(word.letters, {'c':1,'e':2,'l':1,'n':1,'o':1,'r':1,'s':1,'t':3,'u':1})
@@ -44,17 +28,6 @@ class TestWord(unittest.TestCase):
 
         difference2 = word2.difference(word)
         self.assertEqual(difference2, {'a':-2, 'b':-1, 'c':0, 'd':-1})
-
-
-#    def test_difference(self):
-#        word = Word('abbccc')
-#        difference = word.difference(word)
-#        self.assertEqual(difference, [0,0,0])
-#        
-#        word2 = Word('abc')
-#        difference = word.difference(word2)
-#        self.assertEqual(difference, [0,1,2])
-
  
 
 if __name__ == '__main__':
