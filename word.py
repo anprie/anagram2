435a9a106d34b2356ecc02f0bafbb785ef58c43a
Antonia <56763757+antoniapriesnitz@users.noreply.github.com>
2022-10-08 13:41:28 +0200

    remove attribute inventory and method count a nd old code.

--- word.py
+++ word.py
@@ -7,7 +7,6 @@ class Word:
         self.passed = word
         loweralpha = "".join([l.lower() for l in word if l.isalpha()])
         self.word = loweralpha
-        self.inventory = sorted(list(set(loweralpha)))
         self.letters = dict([(l,loweralpha.count(l)) for l in loweralpha])
 
     def __str__(self):
@@ -16,19 +15,6 @@ class Word:
     # counts how many times each of self's unique letters occurs in word 
     # if the input word contains letters that are not in the inventory, return False
     # else return a list of numbers that represent the occurrences of self's letters in word, in the order of inventory
-    def count(self, word):
-        count = [0 for l in self.inventory]
-        not_in_inventory = []
-        for l in word:
-            contains_letter = False
-            for i in range(len(self.inventory)):
-                if l == self.inventory[i]:
-                    count[i]+=1
-                    contains_letter = True
-                    break
-            if not contains_letter:
-                not_in_inventory.append(l)
-        return (count, not_in_inventory)
 
     def difference(self,word):
         l1 = copy.deepcopy(self.letters)
@@ -40,11 +26,6 @@ class Word:
                 l1[key] = -l2[key]
         return l1
 
-
-#    def difference(self, word):
-#        (i_count, i_spare) = self.count(self.word)
-#        (w_count, w_spare) = self.count(word.word)
-#        return [i_count[i]-w_count[i] for i in range(len(i_count))]
 
 
 
