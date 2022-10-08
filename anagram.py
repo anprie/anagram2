66ee5425d52964b61ef111acda8f57db91671fce
Antonia <56763757+antoniapriesnitz@users.noreply.github.com>
2022-10-08 17:25:40 +0200

    remove attributes file and language from class anagram. Adjust print method.

--- anagram.py
+++ anagram.py
@@ -5,13 +5,11 @@ class Anagram:
 
     def __init__(self, word, language_file):
         self.word = word
-        self.file = language_file
-        self.language = language_file.split(sep='.', maxsplit=1)[0]
         self.inventory = sorted(word.letters.keys())
         self.count = [word.letters[key] for key in sorted(word.letters.keys())]
 
     def __str__(self):
-        return f"word: {self.word.word}\nfile: {self.file}\nlanguage: {self.language}"
+        return f"word: {self.word.word}\ninventory: {self.inventory}\noccurrences of letters in inventory: {self.count}"
 
     def subtract(self,word):
         result = copy.deepcopy(self.count)
@@ -27,25 +25,5 @@ class Anagram:
                 not_in_inventory.append(l)
         return (result, not_in_inventory)
 
-    def read(self):
-        # declare three empty sets (onset, nucleus, coda)
-        # open file with clusters
-        #################################
-        # lines look like this:
-        # mfp 0 0 1
-        # äu 1 1 1
-        # st 1 0 1
-        # str 1 0 0
-        #################################
-        # parse lines: 
-        #   split into cluster, o, n, c
-        #   create Word object of cluster
-        #   test if cluster letters are subset of word letters
-        #   if yes:
-        #       1 1 1 -> nucleus
-        #       1 0 0 -> onset
-        #       0 0 1 -> coda
-        #       1 0 1 -> coda and onset
-        pass
 
 
