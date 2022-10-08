489882dad0a9e3521c6351a429a4581699379bd1
Antonia <56763757+antoniapriesnitz@users.noreply.github.com>
2022-10-08 20:11:27 +0200

    adjust arguments to Anagram constructor in tests.

--- test_anagram.py
+++ test_anagram.py
@@ -4,25 +4,23 @@ from word import Word
 class TestAnagram(unittest.TestCase):
 
     def test_inventory(self):
-        word = Word('dcbaABCD')
-        anagram = Anagram(word, 'german.txt')
-        print(anagram)
+        anagram = Anagram(Word('bcbcba'), Language('smurf.txt'))
         self.assertEqual(anagram.inventory,['a','b','c','d'])
 
     def test_count(self):
-        word = Word('bcbcba')
-        anagram = Anagram(word, 'german.txt')
-        print(anagram)
+        anagram = Anagram(Word('bcbcba'), Language('smurf.txt'))
         self.assertEqual(anagram.count, [1,3,2])
 
     def test_subtract(self):
-        word = Word('bcbcba')
-        anagram = Anagram(word, 'german.txt')
+        anagram = Anagram(Word('bcbcba'), Language('smurf.txt'))
         word2 = Word('bcbxy')
         (difference, spare) = anagram.subtract(word2)
         self.assertEqual(difference, [1,1,1])
         self.assertEqual(spare, ['x','y'])
 
+    def test_language(self):
+        anagram = Anagram(Word('bcbcba'), Language('smurf.txt'))
+        self.assertEqual(self.language.nucleus, {'u'})
 
 if __name__ == '__main__':
     unittest.main()
